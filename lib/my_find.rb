9bb1a1ec7f5c3ae9fd6bdca145471a7da71ee8f8
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-06 04:29:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,9 @@
 def my_find(collection)
- i = 0
+i = 0
   while i < collection.length
-  yield(collection[i]) 
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
   end
 end
